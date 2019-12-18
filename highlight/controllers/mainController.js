7286bce4ad2997f3663968da443260f4dbe82c03
Patricia Martins <pmartins@PatoMartins.local>
2019-12-17 21:52:45 -0300

    cambio a ejs

--- highlight/controllers/mainController.js
+++ highlight/controllers/mainController.js
@@ -1,25 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
-// ************ Function to Read an HTML File ************
-function readHTML (fileName) {
-	let filePath = path.join(__dirname, `/../views/${fileName}.html`);
-	let htmlFile = fs.readFileSync(filePath, 'utf-8');
-	return htmlFile;
-}
+// // ************ Function to Read an HTML File ************
+// function readHTML (fileName) {
+// 	let filePath = path.join(__dirname, `/../views/${fileName}.html`);
+// 	let htmlFile = fs.readFileSync(filePath, 'utf-8');
+// 	return htmlFile;
+// }
 
 const controller = {
 	root: (req, res) => {
-		let html = readHTML('index');
-		res.send(html);
+		res.render("index");
 	},
 	root_carrito: (req, res) => {
-		let html = readHTML('carrito');
-		res.send(html);
+		res.render('carrito');
 	},
 	root_detalle: (req, res) => {
-		let html = readHTML('detalle');
-		res.send(html);
+		res.render('detalle');	
 	},
 	
 };
