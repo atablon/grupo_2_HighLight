ab5b5a827c069f0400a36919eef9a31375ed6003
Patricia Martins <pmartins@PatoMartins.local>
2019-12-24 00:45:17 -0300

    Ejs

--- highlight/controllers/mainControllerPublicar.js
+++ highlight/controllers/mainControllerPublicar.js
@@ -2,11 +2,16 @@ const fs = require('fs');
 const path = require('path');
 
 
+let productos = [{
+    "nombre": "Cartel"
+}, {
+        "nombre": "Cartelera"
+}]
 
 const controller = {
 
     publicar: (req, res) => {
-        res.render('publicar');
+        res.render('publicar', {productos});
     },
 
 };
