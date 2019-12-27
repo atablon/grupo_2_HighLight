f6897a687d9c352b46b726ef69f99dd6f489a4f0
Patricia Martins <pmartins@PatoMartins.local>
2019-12-26 22:34:58 -0300

    jsom

--- highlight/controllers/mainControllerPublicar.js
+++ highlight/controllers/mainControllerPublicar.js
@@ -1,22 +1,47 @@
 const fs = require('fs');
 const path = require('path');
 
+/* ubicacion del archivo producto json*/
+const ubicacionProductosJSON = path.join(__dirname,'../data/productos_creados.json');
+const contenidoProductosJSON = fs.readFileSync(ubicacionProductosJSON, 'utf-8');
 
-let productos = [{
-    "nombre": "Cartel"
-}, {
-        "nombre": "Cartelera"
-}]
+
 
 const controller = {
 
     publicar: (req, res) => {
-        res.render('publicar', {productos});
+        res.render('publicar');
     },
     detalle: (req, res) => {
         res.render('detalle');
     }, 
     crearproducto : (req, res) => {
+        // array 
+        let array = [];
+
+        // Si el archivo no está vacío 
+        
+        if (contenidoProductosJSON != '') {
+            // tomo el contenido y lo convierto en un formato de Array de objetos literales
+            arrayDeProductos = JSON.parse(contenidoProductosJSON);
+        }
+
+        // Genero el id para el producto
+        req.body = {
+            id: arrayDeProductos.length + 1,
+            ...req.body
+        };
+
+
+        // Inserto el producto nuevo
+        arrayDeProductos.push(req.body);
+
+        // Convierto el arrayDeProductos a JSON
+        let contenidoAGuardar = JSON.stringify(arrayDeProductos, null, ' ');
+
+        // guardo el array completo en el archivo JSON
+        fs.writeFileSync(ubicacionProductosJSON, contenidoAGuardar);
+
         res.send("Listo")
     } 
    
