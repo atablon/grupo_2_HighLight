bfff042f3ce4b5d45f29e2b25e8db0ccfae0e53c
Patricia Martins <pmartins@PatoMartins.local>
2020-01-11 02:42:39 -0300

    publicar productos

--- highlight/routes/main.js
+++ highlight/routes/main.js
@@ -8,7 +8,7 @@ const path = require('path');
 // esto es un middleware podemos sacarlo afuera y llamarlo en cada ruta que necesitemos
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, __dirname + '/../public/images/imagescartel')
+        cb(null, __dirname + '/../public/images/imagescartel') //aca falta pain.join(__dirname, "../../public...")
     },
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
@@ -23,6 +23,9 @@ const mainController = require('../controllers/mainController');
 const mainControllerPublicar = require('../controllers/mainControllerPublicar');
 const mainControllerDetalle = require('../controllers/mainControllerDetalle');
 
+/* para el nuevo publicar por pasos */
+const mainControllerPublicarPasos = require('../controllers/mainControlerPublicarPasos');
+
 
 
 /* GET - home page. */
@@ -34,12 +37,22 @@ router.get('/registrar', mainController.registrar);
 router.get('/detalle', mainControllerDetalle.detalle);
 router.get('/listado', mainControllerPublicar.listado);
 router.get('/products/create', mainControllerPublicar.crear);
+router.get('/products/publicar', mainControllerPublicarPasos.publicarPasoUno);
+
+
+/*POST Para guardar información del PASO UNO de publicar */
+router.post('/products/publicar', upload.any(), mainControllerPublicarPasos.crearPasoUno);
+
+/*POST Para guardar información del PASO DOS de publicar */
+router.post('/products/publicarDos', upload.any(), mainControllerPublicarPasos.crearPasoDos);
+
+
 
 /* Get para ser redirigido a la pagina de detalle */
 router.get('/detalle/:CartelId',mainControllerDetalle.detalle)
 
-/* POST - para guardar un producto nuevo. */
-router.post('/products/create',upload.any(), mainControllerPublicar.crearcartel);
+/* POST - para guardar un producto nuevo. */ 
+router.post('/products/create',upload.any(), mainControllerPublicar.crearcartel);/* si es una sola imagen poner .single(nombrename del input. )
 
 /* DELETE - Borrar un Producto en DB */
 router.delete('/productos/borrar/:id', mainControllerPublicar.borrarCartel);
