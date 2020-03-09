dcf9378843cea0704a639631ba3926952c4daff8
atablon <a.jtablon@gmail.com>
2020-03-09 13:35:58 -0300

    Correcciones y pruebas Db

--- highlight/controllers/signController.js
+++ highlight/controllers/signController.js
@@ -9,14 +9,25 @@ const db = require("../src/database/models")
 
 const controller = {
     index: async (req, res) => {
-        let tech = db.Sign_tech.findAll();
-        let type = db.Sign_type.findAll();
-        let sign = db.Sign.findAll();
+        let tech = db.Sign_tech.findAll({
+          include: ['signs'] 
+        });
+        let type = db.Sign_type.findAll({
+          include: ['signs']
+        });
+        let sign = db.Sign.findAll({
+          include: ['techs','types','users',]//'orders']
+        });
+        let users = db.User.findAll({
+          include: ['signs']
+        });
+        let orders = db.Order.findAll();
         Promise
-        .all([tech, type, sign])
+        .all([tech, type, sign, users, orders])
         .then (results => {
-            console.log(results[0]);
-            return res.render('sign/index', { tech: results[0], type: results[1], sign: results[1]  });
+            console.log(results[4]);
+            //return res.render('sign/index', { tech: results[0], type: results[1], sign: results[2]  });
+            return res.send({tech: results[0], type: results[1], sign: results[2], user: results[3], orders: results[4]});
         })
 
        }
