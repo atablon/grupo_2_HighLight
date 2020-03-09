34db0cca1e9c357c1d1be6463ee2a9e76f5f30d4
Patricia Martins <patomartins@gmail.com>
2020-03-09 13:35:39 -0300

    form-validations

--- highlight/controllers/signController.js
+++ highlight/controllers/signController.js
@@ -6,44 +6,34 @@ const db = require("../src/database/models")
 
 /*********************************************************************************/
 
-
-<<<<<<< HEAD
-    index: (req, res) => {
-      //  res.send('hola');
-       
-        db.Sign
-        .findAll({
-          include: ['users']
-        })
-        .then(resultados => {
-              return res.send(resultados);
-              console.log(resultados);
-              res.render('sign/index',{resultados});
-              
-           })
-         .catch(function (error) {
-            //console.log(error)
-           })
-         
-    }
-=======
-
 const controller = {
-    index: async (req, res) => {
-        let tech = db.Sign_tech.findAll();
-        let type = db.Sign_type.findAll();
-        let sign = db.Sign.findAll();
-        Promise
-        .all([tech, type, sign])
-        .then (results => {
-            console.log(results[0]);
-            return res.render('sign/index', { tech: results[0], type: results[1], sign: results[1]  });
-        })
-
-       }
-}
+    index: (req, res) => {
+        res.send('hola');
        
->>>>>>> 289109001fef8ed0f99b811a5c2c007984d2e41d
+        // db.Sign
+        // .findAll({
+        //   include: ['users']
+        // })
+        // .then(resultados => {
+        //       return res.send(resultados);
+        //       console.log(resultados);
+        //       res.render('sign/index',{resultados});
+              
+        //    })
+        //  .catch(function (error) {
+        //     //console.log(error)
+        //    })
+         
+        }, 
+        publish: (req, res) => {
+          res.render("sign/viewPublish"); 
+        }, 
+        sign_list: (req, res) => {
+          res.send("Publicado")
+        }, 
+
+
+}
 
 
 module.exports = controller
